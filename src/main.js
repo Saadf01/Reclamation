5a343ef901fb71fdfb1160286fc166b075fc7a3e
bendadouch <you@example.com>
2024-07-10 14:04:48 +0100

    header du page d'acceuil

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import {fas} from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(fas)
 
 const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+app.component('font-awesome-icon', FontAwesomeIcon).mount('#app');
--- src/pages/Accueil_Page.vue
+++ src/pages/Accueil_Page.vue
@@ -1,5 +1,24 @@
 <template>
-    <h1>Bonjour, vous êtes sur la page d'accueil</h1>
+  <div class="header">
+    <div class="lateral">
+      <span class="omrane">Al omrane</span>
+    </div>
+    <div class="rec">
+     <span class="reclamation">Gestion des reclamations</span>
+    </div>
+    <div class="menu">
+      <div class="select">
+     <select name="menu" id="menuser">
+      <option value="user">Acceuil </option>
+      <option value="dog">profil</option>
+      <option value="cat">parametres</option>
+      <option value="parrot">Tableau de bord</option>
+      <option value="hamster">deconnexion</option>
+  </select>
+    <font-awesome-icon :icon="['fas', 'caret-down']" class="icon"/>
+</div>
+</div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +26,43 @@ export default {
   name: 'AccueilPage'
 }
 </script>
+<style>
+body{
+  margin:0px;
+  font-size: 15px;
+}
+.lateral,.rec,.menu{
+  display:inline-block;
+}
+.lateral{
+  padding:15px;
+  background-color: green;
+  height: 20px;
+}
+.rec{
+  margin-left : 20px; 
+}
+.menu{
+  margin-left:950px;
+}
+#menuser{
+  border:none;
+  appearance: none;
+  padding:0 30px 0 15px;
+  width:100%  ;
+  color:white;
+  background-color:#0dd345;
+  height: 20px;
+}
+.header{
+background-color:#0dd345;
+height:50px;
+color:white;
+}
+
+.menu .icon{
+  position: absolute;
+  right: 60px;
+  pointer-events: none;
+}       
+</style>
