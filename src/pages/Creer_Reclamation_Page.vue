635cfc00a34ec0afe0bc509c050c61caa37d70f5
Saadf01 <saadaniqfilali@gmail.com>
2024-07-11 20:22:40 +0100

    SideBar

---
+++ src/pages/Creer_Reclamation_Page.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Créer une réclamation</h1>
+</template>
+
+<script>
+export default {
+  name: 'Creer_Reclamation_Page'
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,25 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Accueil from '@/pages/Accueil_Page.vue'
+import CreerReclamation from '@/pages/Creer_Reclamation_Page.vue'
+import ListeReclamations from '@/pages/Liste_Reclamations_Page.vue'
+
 
 const routes = [
     {
         path: '/',
         name: 'Accueil',
         component: Accueil
-    }
+    },
+    {
+        path: '/CreerReclamation',
+        name: 'CreerReclamation',
+        component: CreerReclamation
+    },
+    {
+        path: '/ListeReclamations',
+        name: 'ListeReclamations',
+        component: ListeReclamations
+    },
 ]
 
 const router = createRouter({
